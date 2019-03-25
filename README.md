fc132663eb05b5c5a9afe3f6e85184f4a6ec609f
vmuthabuku <vmuthabuku@gmail.com>
2019-03-25 03:59:27 +0300

    Add project

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # vmuthabuku
-my-site
+[![Netlify Status](https://api.netlify.com/api/v1/badges/346037cb-a065-442a-939f-15bc00415670/deploy-status)](https://app.netlify.com/sites/vmuthabuku/deploys)
--- src/projects.js
+++ src/projects.js
@@ -1,37 +1,44 @@
 export const portfolio = [
-    {
-        title: "Pixel-Art",
-        type:"Front-End",
-        description: "udacity pixel-art maker project written in jquery",
-        sourcecodeurl: "https://github.com/vmuthabuku/pixel-art-jquery",
-        site: "https://vmuthabuku.github.io/pixel-art-jquery/index.html"
-      },
-      {
-        title: "Questioner",
-        type:"Front-End",
-        description: "A website written in pure html and css no frame works used",
-        sourcecodeurl: "https://github.com/vmuthabuku/Questioner",
-        site: "https://vmuthabuku.github.io/Questioner/UI/"
-      },
-      {
-        title: "Node-js trial",
-        type:"Front-End + Backend",
-        description: "This is a simple trial node js app that uses socket io and mongo db ",
-        sourcecodeurl: "https://github.com/vmuthabuku/nodejs-jquery-simpleapp",
-        site: "https://vmuthabuku.github.io/nodejs-jquery-simpleapp/"
-      },    
+  {
+    title: "Game-Review",
+    type:"Backend",
+    description: "A website written in node js ",
+    sourcecodeurl: "https://github.com/vmuthabuku/Game-Review"
+  },
+  {
+    title: "Node-js trial",
+    type:"Front-End + Backend",
+    description: "This is a simple trial node js app that uses socket io and mongo db ",
+    sourcecodeurl: "https://github.com/vmuthabuku/nodejs-jquery-simpleapp",
+    site: "https://vmuthabuku.github.io/nodejs-jquery-simpleapp/"
+  },    
+  {
+    title: "Questioner",
+    type:"Front-End",
+    description: "A website written in pure html and css no frame works used",
+    sourcecodeurl: "https://github.com/vmuthabuku/Questioner",
+    site: "https://vmuthabuku.github.io/Questioner/UI/"
+  },
 
-    {
-        title: "Store-Manager-api",
-        type:"Backend",
-        description: "This is a Store Manager API written in python and tested using travis Ci",
-        sourcecodeurl: "https://github.com/vmuthabuku/Store-Manager"
-      },
-      {
-        title: "Questioner-Api",
-        type:"Backend",
-        description: "This is a Questioner API written in python and tested using travis Ci",
-        sourcecodeurl: "https://github.com/vmuthabuku/Questioner-api-v1",
-      }
+{
+    title: "Store-Manager-api",
+    type:"Backend",
+    description: "This is a Store Manager API written in python and tested using travis Ci",
+    sourcecodeurl: "https://github.com/vmuthabuku/Store-Manager"
+  },
+  {
+    title: "Questioner-Api",
+    type:"Backend",
+    description: "This is a Questioner API written in python and tested using travis Ci",
+    sourcecodeurl: "https://github.com/vmuthabuku/Questioner-api-v1",
+  },
+  {
+      title: "Pixel-Art",
+      type:"Front-End",
+      description: "udacity pixel-art maker project written in jquery",
+      sourcecodeurl: "https://github.com/vmuthabuku/pixel-art-jquery",
+      site: "https://vmuthabuku.github.io/pixel-art-jquery/index.html"
+    }
+     
 ]
    
